baf674c0cdd1952b61b4e25f7cd96ab49080058e
oumarissasale <47788100+oumarissasale@users.noreply.github.com>
2019-05-11 16:19:54 +0100

    Add files via upload

---
+++ CS50 Pset3/helpers.h
@@ -0,0 +1,11 @@
+// Oumarou issa sale 
+// Helper functions for music
+
+#include <cs50.h>
+
+// the Convertion of a fraction to eighths
+int duration(string fraction);
+
+   int frequency(string note);
+    bool is_rest(string s);
+
